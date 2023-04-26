63ec687c78f822ba2fc6ee31befe93f5c0d48dda
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-04-26 14:56:21 +0700

    node modules added to gitignore

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+app.use(cors())
 
 // create routes
 app.get('/', (req, res) => {
   res.send('Welcome Google Search')
 })
+
+// create results route
+app.get('/results', (req, res) => {})
 
 app.listen(4000)
