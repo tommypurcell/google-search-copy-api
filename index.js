b9cb88c8d415d818536e016377e5805e80ebcb3e
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-04-26 15:17:47 +0700

    corrected spelling in gitignore for node_modules

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get('/', (req, res) => {
   res.send('Welcome Google Search')
 })
 
+app.get('/results', (req, res) => {
+  let str = req.query
+  function search(e, str) {
+    e.preventDefault()
+    let matchingResults = []
+
+    const filteredResults = data.filter((result) =>
+      result.title.toLowerCase().includes(str.toLowerCase())
+    )
+    setResults(filteredResults)
+
+    // if search bar is empty dont show any results
+    if (str == '') {
+      let emptyArr = []
+      setResults(emptyArr)
+    }
+    console.log(filteredResults)
+    console.log(results)
+  }
+})
+
 // create results route
 app.get('/results', (req, res) => {})
 
