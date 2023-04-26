3f2197e45aa15e31a15bee5b48962075c331322e
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-04-26 16:07:10 +0700

    api will filter results now with query

--- index.js
+++ index.js
@@ -9,27 +9,74 @@ app.get('/', (req, res) => {
 })
 
 app.get('/results', (req, res) => {
-  let str = req.query
-  function search(e, str) {
-    e.preventDefault()
-    let matchingResults = []
+  let str = req.query.search
+  console.log(str.toLowerCase())
+  let data = [
+    {
+      title: 'JS testing',
+      description: 'The best JavaScript tutorials in the galaxy!',
+      url: 'www.testing.com',
+      links: [
+        {
+          title: 'JS for Beginner tests',
+          url: 'https://www.w3schools.com/js',
+        },
+        {
+          title: 'JS for the Web',
+          url: 'https://www.w3schools.com/js',
+        },
+      ],
+    },
+    {
+      title: 'Tutorial - W3School',
+      description:
+        'Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL...',
+      url: 'developer.mozilla.org',
+      links: [
+        {
+          title: 'JS Introduction',
+          url: 'https://www.w3schools.com/js/js_intro.asp',
+        },
+        {
+          title: 'JS Functions',
+          url: 'https://www.w3schools.com/js/js_functions.asp',
+        },
+      ],
+    },
+    {
+      title: 'JavaScript | MDN',
+      description:
+        'JavaScript is the programming language of the Web. JavaScript is easy to learn. This tutorial will teach you JavaScript from basic to advanced. Start learning ...',
+      url: 'developer.mozilla.org',
+      links: [
+        {
+          title: 'JavaScript Operator',
+          url: 'https://www.w3schools.com/js/js_examples.asp',
+        },
+        {
+          title: 'JavaScript Code',
+          url: 'https://www.w3schools.com/js/js_functions.asp',
+        },
+        {
+          title: 'JavaScript Meaning',
+          url: 'https://www.w3schools.com/js/js_functions.asp',
+        },
+      ],
+    },
+  ]
 
-    const filteredResults = data.filter((result) =>
-      result.title.toLowerCase().includes(str.toLowerCase())
-    )
-    setResults(filteredResults)
+  let filteredResults = data.filter((result) =>
+    result.title.toLowerCase().includes(str)
+  )
 
-    // if search bar is empty dont show any results
-    if (str == '') {
-      let emptyArr = []
-      setResults(emptyArr)
-    }
-    console.log(filteredResults)
-    console.log(results)
-  }
+  // if search bar is empty dont show any results
+  // if (str == '') {
+  //   let emptyArr = []
+  //   return emptyArr
+  // }
+
+  //   let resultsList = search()
+  res.send(filteredResults)
 })
-
-// create results route
-app.get('/results', (req, res) => {})
 
 app.listen(4000)
